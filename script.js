5f18b498e4fe338df0ec345d11db9997e311ca5b
anantharam kalepu <csrinsightsin@gmail.com>
2025-09-28 22:07:46 +0100

    WCSR-1: basic website structure.

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
     const mobileMenuToggle = document.getElementById('mobile-menu-toggle');
     const mainNav = document.querySelector('.main-nav');
 
-    mobileMenuToggle.addEventListener('click', function() {
-        mainNav.classList.toggle('active');
-    });
+    if (mobileMenuToggle) {
+        mobileMenuToggle.addEventListener('click', function() {
+            mainNav.classList.toggle('active');
+        });
+    }
 });
